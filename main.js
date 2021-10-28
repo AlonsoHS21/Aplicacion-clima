aac3dfe1e8caa8ae7a8a44db945d162bcbc9e2c0
Alonso <61801479+AlonsoHS21@users.noreply.github.com>
2021-10-27 22:37:09 -0300

    Error arreglado ya pude hacer que traiga los datos desde la API me saltaba error 401
    El error esta explicado en main.js

--- main.js
+++ main.js
@@ -5,16 +5,24 @@ const API_KEY = 'b2885e62e8e55a917a5e9b82390c87cb';
 const getWeatherData = position => {
     //Dentro del objeto coords copiamos el contenido que tengan como clave el mismo nombre a las variables llamadas latitude y longitude
     const { latitude , longitude } = position.coords;
-
     //fetch() nos permite obtener recursos de forma asincronica por la red, en este caso lo estoy usando para llamar a la API de OpenWeather
-    fetch('http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}')
+    fetch(`http://api.openweathermap.org/data/2.5/weather?units=metric&lat=${latitude}&lon=${longitude}&appid=${API_KEY}`) // 1 - ERROR
     .then(response => response.json()) //Lo que hacemos es extraer la respuesta en formato JSON
-    .then(data => console.log(data)) //Mostramos los datos ya en formato JSON recibidos del anterior then
+    .then(data => setWeatherData(data)) //A la funcion setWeatherData le envio como parametro los datos traidos desde la API
    
-    //Mostramos la posicion para saber lo que se obtiene
-    console.log(position); 
+    const setWeatherData = data => {
+         //Mostramos la data para saber lo que se obtiene
+        console.log(data); 
+    }
+   
 }
 // Es funcion sirve para obtener la ubicacion del usuario
 const onLoad = () => {
     navigator.geolocation.getCurrentPosition(getWeatherData);
 }
+
+/*
+    1- ERROR: Mucho ojo confundir las comillas simples con "acento grave" (alt + 96) segun ascii, es un error de sintaxis me costo mucho darme cuenta
+    2-
+    3- 
+*/ 
